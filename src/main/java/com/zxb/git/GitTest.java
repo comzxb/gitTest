6b114e4329db36810c652296cc0cbe4c4ef7e3ce
zxb <3295057890@qq.com>
2022-10-14 09:31:33 +0800

    hot-fix diyici

--- src/main/java/com/zxb/git/GitTest.java
+++ src/main/java/com/zxb/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaa");
         System.out.println("bbbbbbbbbbbbbb");
+        System.out.println("cccccccccccccccccccccc");
     }
 }
